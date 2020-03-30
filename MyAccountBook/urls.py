af21460f4b438094819967dc286a0e7b9a9cccb3
akhilkarkera <akhilskarkera@gmail.com>
2020-03-30 19:28:27 +0530

    updated for testing

--- MyAccountBook/urls.py
+++ MyAccountBook/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 
-    Test commit - 2 from app
+    Test commit - 2 
 """
 
 from django.contrib import admin
